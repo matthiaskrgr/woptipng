0aa4fd7ddd201ee0d56c4146d60dbf581ad842b2
Matthias Krüger <matthias.krueger@famsik.de>
2022-05-06 22:40:19 +0200

    input validation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
 use clap::{AppSettings, Arg, ArgMatches};
 
+use std::fs::File;
+use std::path::PathBuf;
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -18,7 +21,23 @@ struct Args {
 
 fn main() {
     let cli = Args::parse();
-    let input_paths = cli.paths;
+    let input_paths = cli.paths.iter().map(PathBuf::from).collect::<Vec<_>>();
 
-    dbg!(input_paths);
+    validate_input_paths(&input_paths)
+}
+
+/// check that all input paths are present/valid, if not, terminate
+fn validate_input_paths(input_paths: &[PathBuf]) {
+    let invalid_paths = input_paths
+        .iter()
+        .filter(|path| !path.exists())
+        .collect::<Vec<_>>();
+    if !invalid_paths.is_empty() {
+        eprintln!("Warning: the following files could not be found:");
+        invalid_paths
+            .iter()
+            .for_each(|p| eprint!("{}, ", p.display()));
+        eprintln!();
+        std::process::exit(1);
+    }
 }
